9372d49cde6f549307a15594ea04abb6a6e21338
Muhammad Furqan <furqan@tokopedia.com>
2022-04-04 02:40:53 +0700

    Camera Complete

--- app/src/main/java/com/furqan/intermediateandroid/media/MediaActivity.kt
+++ app/src/main/java/com/furqan/intermediateandroid/media/MediaActivity.kt
@@ -1,14 +1,41 @@
 package com.furqan.intermediateandroid.media
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import com.furqan.intermediateandroid.R
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.furqan.intermediateandroid.databinding.ActivityMediaBinding
 
 class MediaActivity : AppCompatActivity() {
+
+    private val activityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val image = it.data?.extras?.get("data") as Bitmap?
+                image?.let { bitmap ->
+                    binding.ivThumbnail.setImageBitmap(bitmap)
+                }
+            }
+        }
+
+    private lateinit var binding: ActivityMediaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMediaBinding.inflate(layoutInflater)
+        binding = ActivityMediaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        with(binding) {
+            binding.btnCamera.setOnClickListener {
+                try {
+                    activityResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+                } catch (t: Throwable) {
+                    Toast.makeText(this@MediaActivity, t.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
