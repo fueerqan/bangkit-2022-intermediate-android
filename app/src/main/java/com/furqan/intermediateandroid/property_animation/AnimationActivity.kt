0d08e11b3a4ef3db5bd930755d95c42ebf83d6be
Muhammad Furqan <furqan@tokopedia.com>
2022-04-04 00:23:56 +0700

    Object Animation

--- app/src/main/java/com/furqan/intermediateandroid/property_animation/AnimationActivity.kt
+++ app/src/main/java/com/furqan/intermediateandroid/property_animation/AnimationActivity.kt
@@ -1,13 +1,43 @@
 package com.furqan.intermediateandroid.property_animation
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.furqan.intermediateandroid.databinding.ActivityAnimationBinding
+import kotlin.random.Random
 
 class AnimationActivity : AppCompatActivity() {
+
+    companion object {
+        private const val MIN_TRANSLATION = 100f
+        private const val MAX_TRANSLATION = 500f
+
+        private val TRANSLATION_OPTION = listOf(
+            "translationX",
+            "translationY"
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityAnimationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        with(binding) {
+            root.setOnClickListener {
+                val translation = Random.nextInt(0, TRANSLATION_OPTION.size)
+                ObjectAnimator.ofFloat(
+                    tvCatchMe,
+                    TRANSLATION_OPTION[translation],
+                    randomPosition()
+                ).apply {
+                    duration = 500
+                    start()
+                }
+            }
+        }
     }
+
+    private fun randomPosition(): Float =
+        MIN_TRANSLATION + Random.nextFloat() * (MAX_TRANSLATION - MIN_TRANSLATION)
 }
