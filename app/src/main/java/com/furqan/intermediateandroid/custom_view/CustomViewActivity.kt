b719f90f9aa7049f9c1183fdd973fb84fcadc9e6
Muhammad Furqan <furqan@tokopedia.com>
2022-04-03 21:27:58 +0700

    Manual View

--- app/src/main/java/com/furqan/intermediateandroid/custom_view/CustomViewActivity.kt
+++ app/src/main/java/com/furqan/intermediateandroid/custom_view/CustomViewActivity.kt
@@ -1,12 +1,26 @@
 package com.furqan.intermediateandroid.custom_view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.furqan.intermediateandroid.R
+import androidx.appcompat.app.AppCompatActivity
+import com.furqan.intermediateandroid.databinding.ActivityCustomViewBinding
 
 class CustomViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_custom_view)
+        val binding = ActivityCustomViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            // manual setup
+            btnSubstract.setOnClickListener {
+                val counter = edtCounter.text.toString().toInt() - 1
+                edtCounter.setText(counter.toString())
+            }
+
+            btnPlus.setOnClickListener {
+                val counter = edtCounter.text.toString().toInt() + 1
+                edtCounter.setText(counter.toString())
+            }
+        }
     }
 }
