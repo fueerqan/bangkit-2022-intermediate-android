e7ee33028e9a08f9ead0c19600639563fac4f600
Muhammad Furqan <furqan@tokopedia.com>
2022-04-04 15:02:12 +0700

    get Locale

--- app/src/main/java/com/furqan/intermediateandroid/MainActivity.kt
+++ app/src/main/java/com/furqan/intermediateandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.furqan.intermediateandroid
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.furqan.intermediateandroid.canvas.CanvasActivity
 import com.furqan.intermediateandroid.custom_view.CustomViewActivity
@@ -9,12 +10,17 @@ import com.furqan.intermediateandroid.databinding.ActivityMainBinding
 import com.furqan.intermediateandroid.media.MediaActivity
 import com.furqan.intermediateandroid.motion_layout.MotionLayoutActivity
 import com.furqan.intermediateandroid.property_animation.AnimationActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val locale = Locale.getDefault()
+        Toast.makeText(this, locale.language, Toast.LENGTH_SHORT).show()
+
 
         with(binding) {
             btnGoToCustomView.setOnClickListener {
